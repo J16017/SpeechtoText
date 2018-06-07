5069e02bce8c1fe034a35152a9a8bfba8bb7ce7c
taiga <taiga@J16017>
2018-06-07 12:37:55 +0900

    yes

--- src/speechtotext/SpeechtoText_main.java
+++ src/speechtotext/SpeechtoText_main.java
@@ -17,8 +17,11 @@ public class SpeechtoText_main {
 	    service.setUsernameAndPassword("J16017", "J16017");
 
 	    File audio = new File("audio/output.wav");
-
 	    RecognizeOptions options = null;
+
+	    MySQL mysql=new MySQL();
+	    mysql.updateImage("a",0.5);
+
 		try {
 			options = new RecognizeOptions.Builder()
 			       .model("ja-JP_BroadbandModel")
@@ -30,19 +33,21 @@ public class SpeechtoText_main {
 			e.printStackTrace();
 		}
 	        SpeechRecognitionResults transcript = service.recognize(options).execute();
-
-	        JsonNode node=null;
+	        System.out.println(transcript);
+	        String s=String.valueOf(transcript);
 	        ObjectMapper mapper=new ObjectMapper();
 	        try{
-	        	node=mapper.readTree(transcript.toString());
+	        	JsonNode node=mapper.readTree(s);
+	        	for(int i=0;i<node.get("results").size();i++){
+	        String text=node.get("results").get(i).get("alternatives").get(0).get("transcript").asText();
+	        System.out.println("transcript :"+text);
+	        double confidence=node.get("results").get(i).get("alternatives").get(0).get("confidence").asDouble();
+	        System.out.println("confidence :"+confidence);
+	        mysql.updateImage(text, confidence);
+	        	}
 	        }catch(IOException e){
 	        	e.printStackTrace();
 	        }
-	        System.out.println(transcript);
-	        String transcript1=node.get("results").get(0).get("alternatives").get(0).get("transcript").asText();
-	        System.out.println(transcript1);
-	        Double confidence=node.get("results").get(0).get("alternatives").get(0).get("confidence").asDouble();
-	        System.out.println(confidence);
 
 	}
 }
